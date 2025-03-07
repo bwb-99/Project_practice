0b690f5673de584ff0e549cfdf65b41a9768e65d
bae <bottleright10@gmail.com>
2025-03-07 19:35:04 +0900

    2025-03-07[메인페이지출력]

--- CocktailProject/src/main/java/com/sist/dao/CocktailDAO.java
+++ CocktailProject/src/main/java/com/sist/dao/CocktailDAO.java
@@ -1,6 +1,7 @@
 package com.sist.dao;
 import java.util.*;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import com.sist.vo.*;
@@ -10,6 +11,60 @@ public class CocktailDAO {
 	static
 	{
 		ssf=CreateSqlSessionFactory.getSsf();
+	}
+	/*
+	 * <select id="cocktailMainHouseData" resultType="CocktailVO">
+	  SELECT * FROM (SELECT * FORM cocktail
+	    ORDER BY DBMS_RANDOM.RANDOM
+	  )
+	  WHERE rownum&lt;=1
+	</select>
+	<select id="cocktailMainHouseData8" resultType="CocktailVO">
+	    SELECT * FROM (SELECT * FROM cocktail
+	      ORDER BY DBMS_RANDOM.RANDOM
+	    )
+	    WHERE rownum&lt;=8
+	  </select>
+	</mapper>
+	 */
+	public static CocktailVO cocktailMainHouseData()
+	{
+		CocktailVO vo=null;
+		SqlSession session=null;
+		try
+		{
+			session=ssf.openSession();
+			vo=session.selectOne("cocktailMainHouseData");
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			if(session!=null)
+				 session.close();
+		}
+		return vo;
+	}
+	public static List<CocktailVO> cocktailMainHouseData8()
+	{
+		List<CocktailVO> list=null;
+		SqlSession session=null;
+		try 
+		{
+			session=ssf.openSession();
+			list=session.selectList("cocktailMainHouseData8");
+			
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			if(session!=null)
+				  session.close();
+		}
+		return list;
 	}
 
 }
--- CocktailProject/src/main/java/com/sist/model/MainModel.java
+++ CocktailProject/src/main/java/com/sist/model/MainModel.java
@@ -5,12 +5,22 @@ import com.sist.controller.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.*;
+import com.sist.vo.*;
+import com.sist.dao.*;
 @Controller
 public class MainModel {
 	@RequestMapping("main/main.do")
 	  public String main_main(HttpServletRequest request,HttpServletResponse response)
 	  {
-		request.setAttribute("main_jsp", "../main/home.jsp");
+		CocktailVO vo=CocktailDAO.cocktailMainHouseData();
+		List<CocktailVO> cList=CocktailDAO.cocktailMainHouseData8();
+
+
+		request.setAttribute("cvo", vo);
+		request.setAttribute("cList", cList);
+		  
 		return "../main/main.jsp";
 		
 	  }
